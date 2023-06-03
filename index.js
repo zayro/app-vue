a88b3d5c982239ac662d46012c1a4f44308c5621
Marlon Zayro Arias Vargas <Zayro8905@gmail.com>
2023-06-03 00:03:28 -0500

    🐛resolviendo bugs de despliegue

--- index.js
+++ index.js
@@ -1,6 +1,20 @@
 const express = require('express')
+const figlet = require('figlet')
+const boxen = require('boxen')
+const chalk = require('chalk')
 const path = require('path')
 const app = express()
+
+console.log(chalk.yellow(figlet.textSync('FrontEnd - APP', { horizontalLayout: 'full' })))
+
+console.log(
+  boxen('bY: Marlon Zayro Arias Vargas <zayro8905@gmail.com>', {
+    padding: 1,
+    margin: 1,
+    borderStyle: 'double',
+    borderColor: 'yellow'
+  })
+)
 
 app.use('/', express.static(path.join(__dirname, './dist')))
 
